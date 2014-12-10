e834655f6f30347755104983034c5c7fecd8def4
Jerick Don San Juan <jerick@icannhas.com>
2014-12-10 08:47:09 +0800

    changed templates

--- lab_student_draft/labs/views.py
+++ lab_student_draft/labs/views.py
@@ -1,33 +1,32 @@
 from django.core.urlresolvers import reverse
-from crudwrapper.views import UpdateWithInlinesView, InlineFormSet
-from crudwrapper import CrispyModelForm
-from .models import Lab, Share
-from students.models import Selection
-
-
-class SelectionForm(CrispyModelForm):
-    readonly_fields = ('student_group')
-
-    class Meta:
-        model = Selection
-        fields = ('student_group', 'is_selected')
-
+from braces.views import SetHeadlineMixin
+from crudwrapper.views import (
+    UpdateWithInlinesView, InlineFormSet
+)
+from tables2_extras import ModelSingleTableView
+from .models import Lab
+from students.models import Selection, StudentGroup
 
 
 class SelectionInline(InlineFormSet):
     model = Selection
-    form_class = SelectionForm
-    # fields = ('is_selected', 'student_group')
+    fields = ('is_selected',)
     can_delete = False
     extra = 0
 
 
-class SelectGroupView(UpdateWithInlinesView):
+class SelectGroupView(SetHeadlineMixin, UpdateWithInlinesView):
     model = Lab
     fields = []
     inlines = [SelectionInline]
+    template_name = 'labs/select_group.html'
+    headline = 'Select Groups'
 
     def get_success_url(self):
         return reverse('select-group', kwargs=(dict(pk=self.object.pk)))
+
+    def get_form_valid_message(self):
+        return 'Thank you for your selection!'
+
 
 select_group = SelectGroupView.as_view()
--- lab_student_draft/students/views.py
+++ lab_student_draft/students/views.py
@@ -1,22 +1,28 @@
 from django.core.urlresolvers import reverse
+from braces.views import SetHeadlineMixin
 from crudwrapper.views import UpdateWithInlinesView, InlineFormSet
 from .models import StudentGroup, Student, GroupPreference
 
 
 class PreferenceInline(InlineFormSet):
     model = GroupPreference
-    fields = ('preference', 'lab')
+    fields = ('preference',)
     can_delete = False
     extra = 0
 
 
-class RankPreferenceView(UpdateWithInlinesView):
+class RankPreferenceView(SetHeadlineMixin, UpdateWithInlinesView):
     model = StudentGroup
     inlines = [PreferenceInline]
     fields = []
+    template_name = 'students/rank-preference.html'
+    headline = "Specify you Lab Preferences"
 
     def get_success_url(self):
         return reverse('rank-preference', kwargs=(dict(pk=self.object.pk)))
+
+    def get_form_valid_message(self):
+        return "Thank you for specifying your Lab choices!"
 
 
 class StudentInline(InlineFormSet):
@@ -25,13 +31,18 @@ class StudentInline(InlineFormSet):
     extra = 0
 
 
-class UpdateStudentGroupView(UpdateWithInlinesView):
+class UpdateStudentGroupView(SetHeadlineMixin, UpdateWithInlinesView):
     model = StudentGroup
     inlines = [StudentInline]
     fields = []
+    formset_panel_layout = True
+    headline = "Update Group Members"
 
     def get_success_url(self):
         return reverse('update-group', kwargs=(dict(pk=self.object.pk)))
+
+    def get_form_valid_message(self):
+        return "Group information updated!"
 
 rank_preference = RankPreferenceView.as_view()
 update_group = UpdateStudentGroupView.as_view()
